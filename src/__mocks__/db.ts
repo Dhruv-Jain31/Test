dbe5ce0b9e2d060677eb918280ce1082a32c5237
Dhruv-Jain31 <jain.dhruv3103@gmail.com>
2025-11-19 01:10:36 +0530

    added deep mocking

--- src/__mocks__/db.ts
+++ src/__mocks__/db.ts
@@ -1,23 +1,26 @@
 import { vi } from 'vitest';
+import { mockDeep } from 'vitest-mock-extended';
+import { PrismaClient } from '@prisma/client';
 
 // in the mocking below, we are replacing the prismaClient.request.create method with a mock function
 // so that when the server code calls prismaClient.request.create, it will call this mock function instead of the real one
 // this prevents actual database operations during tests and allows us to verify that the method was called correctly
 // mock is file specific so it will only affect tests in this file
-vi.mock('../db', () => {
+
+/*vi.mock('../db', () => {
     return {
         prismaClient: {
             request: {
                 create : vi.fn(),
                 update : vi.fn(),
             }
-            /*user: {  // if we more models eg : user then we can mock them like this but mock will be very deep then so we do deep mocking
-                findUnique: vi.fn(),
-                create: vi.fn(),
-            }*/
+            // user: {  // if we more models eg : user then we can mock them like this but mock will be very deep then so we do deep mocking
+            //     findUnique: vi.fn(),
+            //     create: vi.fn(),
+            // }
         }
     }
-})
+}); */
 
 
 // but in deep mocking every method inside prismaClient can be mocked individually so that it does not throw errors during tests
@@ -26,3 +29,5 @@ vi.mock('../db', () => {
 // this is useful when the code under test calls multiple methods on the prismaClient instance
 // deep mocking can be achieved using libraries like jest-mock-extended or by manually creating a deep mock object
 // but here we are using simple mocking for demonstration purposes
+
+export const prismaClient = mockDeep<PrismaClient>();
